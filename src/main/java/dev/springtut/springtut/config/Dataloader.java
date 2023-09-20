f707b06ddea2019ec4e02ed7478bd4d68214e93a
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2023-09-20 20:38:32 +0530

    added check for seeding initial data
    Signed-off-by: AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>

--- src/main/java/dev/springtut/springtut/config/Dataloader.java
+++ src/main/java/dev/springtut/springtut/config/Dataloader.java
@@ -26,9 +26,11 @@ public class Dataloader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
-            repository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<Content>>() {
-            }));
+        if (repository.count() == 0) {
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
+                repository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<Content>>() {
+                }));
+            }
         }
     }
 }
